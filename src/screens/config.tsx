350a29a2635c58278a4ff58aed57dfeb36724fb6
Роман Гранков <r.grankov@mediasoft.team>
2021-04-19 10:02:46 +0300

    fix on config

---
+++ src/screens/config.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { LayoutsMain } from '~/layouts/main/LayoutsMain';
+import { Tabs } from '~/components/core/tabs/Tabs';
+
+export const ScreensConfig = () => {
+	return (
+		<LayoutsMain>
+			<Tabs />
+		</LayoutsMain>
+	);
+};
