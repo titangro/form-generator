7593c67615b1ec2a1813a539b864314598060017
Roman Grankov <r.grankov@mediasoft.team>
2021-04-20 00:26:28 +0300

    fixes on deploy, check root route

--- src/screens/config/config.tsx
+++ src/screens/config/config.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
-import { LayoutsMain } from '~/layouts/main/LayoutsMain';
 import { Tabs } from '~/components/core/tabs/Tabs';
 
 export const ScreensConfig = () => {
-	return (
-		<LayoutsMain>
-			<Tabs />
-		</LayoutsMain>
-	);
+	return <Tabs />;
 };
